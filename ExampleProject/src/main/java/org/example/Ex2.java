c637121dea6f8b30217848bc8ac569fc3921c361
Valentina Buenhombre <valentina.buenhombre13@gmail.com>
2024-03-21 15:19:15 -0500

    Commit using intelliJ IDE ultimate

---
+++ ExampleProject/src/main/java/org/example/Ex2.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        System.out.println("2");
+    }
+}
---
+++ ExampleProject/src/main/java/org/example/Ex3.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        System.out.println("3");
+    }
+}
